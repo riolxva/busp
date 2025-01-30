ccb3337bfdd3942fa13c4d01daa6b6ecba93f550
RioL <riolxva@gmail.com>
2025-01-30 21:19:32 +0700

    fdf

--- busp.py
+++ busp.py
@@ -42,10 +42,9 @@ async def echo_handler(message: Message) -> None:
         data = {"initData":"query_id=AAE0DkRoAAAAADQORGg9OzFj&user=%7B%22id%22%3A1749290548%2C%22first_name%22%3A%224s%F0%9F%A4%A7%22%2C%22last_name%22%3A%22%22%2C%22username%22%3A%22xva4s2%22%2C%22language_code%22%3A%22ru%22%2C%22is_premium%22%3Atrue%2C%22allows_write_to_pm%22%3Atrue%2C%22photo_url%22%3A%22https%3A%5C%2F%5C%2Ft.me%5C%2Fi%5C%2Fuserpic%5C%2F320%5C%2F-YLJccB6b9ek6pGSbp3w3rxjTbDtfodmS15XAqHu65c.svg%22%7D&auth_date=1733844565&signature=7on5VhQOH8o2V0dh8wnZIgVIiyvW5aM3nATKQ-3OOJxgnZpB4hw8hDH0eNCOoSUyxubVpy0kw6Kq72MFVD9VDw&hash=4744f6bb621839f00783969f312b38a9afb6410b4a9f24c9f4c9caaa242eaba4"}
         response = requests.post(f"https://buspaybot.icom24.ru/api/search/qr?botName=buspaybot&scannedCode={sp[0]}", json=data)  
         time = datetime.now() + timedelta(hours=7)
-        print(response)
         w = InlineKeyboardMarkup(inline_keyboard=[
-            [InlineKeyboardButton(text="🎫 Предъявить билет", web_app=WebAppInfo(url=f"https://czsygg-89-22-177-128.ru.tuna.am?perevoz={response.json()['basicTripInfo']['carrierName']}&route={response.json()['basicTripInfo']['routeName']}&govno={response.json()['basicTripInfo']['vehicleGovNumber']}&cost={response.json()['tariffs'][0]['tariffValueCent']//100*mul}&date={time.day}&hour={time.hour}&min={time.minute}&count={mul}"))]
-            ])
+            [InlineKeyboardButton(text="🎫 Предъявить билет", web_app=WebAppInfo(url=f'''https://8zbj16-89-22-177-128.ru.tuna.am?perevoz={response.json()['basicTripInfo']['carrierName']}&route={response.json()['basicTripInfo']['routeName'].replace('"','@')}&govno={response.json()['basicTripInfo']['vehicleGovNumber']}&cost={response.json()['tariffs'][0]['tariffValueCent']//100*mul}&date={time.day}&hour={str(time.hour).zfill(2)}&min={str(time.minute).zfill(2)}&count={mul}'''))]
+        ])
         await message.answer(f"Билет куплен успешно.\n{response.json()['basicTripInfo']['carrierName']}\n🚏 {response.json()['basicTripInfo']['routeName']}\n{asd(response)}{response.json()['basicTripInfo']['vehicleGovNumber']}\n🪙 Тариф: Полный {response.json()['tariffs'][0]['tariffValueCent']*mul//100},00 ₽\n🎫 Билет №{rint()}", reply_markup=w)
         await message.delete()
     else:
