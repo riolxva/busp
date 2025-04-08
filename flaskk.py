3c4166c472f3df2f1a9b1ae3ee751bbddb07e3f5
4s33 <topersamp@gmail.com>
2025-04-08 16:15:53 +0300

    some changes

--- flaskk.py
+++ flaskk.py
@@ -1,8 +1,13 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 from random import randint
 import requests
 import json
 from datetime import datetime, timedelta, timezone
+
+
+white = [ 1749290548, 706686986, 1820132315, 1389182288, 752618557, 943904951, 5025077400, 952619704, 630120548, 1690631961, 1429180981 ] 
+#             я          саша      марго     ильягаранин    мать      тимурик     Диана        Дима      володя     серега     петька     
+TOKEN = "7123200792:AAEUI5j0OhDnDObRIGXCN8NEInwSPSEh5z4"
 
 
 def vehicle_type(a):
@@ -25,54 +30,79 @@ def get_vehicle_data(code):
     return response
 
 
+def send_message(chat_id, text):
+    url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
+    payload = {
+        "chat_id": chat_id,
+        "text": text
+    }
+    requests.post(url, json=payload)
+
+
+def delete_message(chat_id, message_id):
+    url = f"https://api.telegram.org/bot{TOKEN}/deleteMessage"
+    payload = {
+        "chat_id": chat_id,
+        "message_id": message_id
+    }
+    requests.post(url, json=payload)
+
+
 app = Flask(__name__)
 
 
-@app.route("/data/", methods=["GET"])
+@app.route("/data/", methods=["POST"])
 def fetch_ticket_data():
-    code = request.args.get("code")
-    count = request.args.get("count")
-    userid = request.args.get("userid")
+    data = request.get_json()
+    if 'message' in data:
+        chat_id = data['message']['chat']['id']
+        message_id = data['message']['message_id']
+        text = data['message'].get('text', '').split()
+        code = text[0]
+        count = int(text[1]) if len(text) == 2 and text[1].isdigit() else 1
 
-    try:
-        with open("codes.json", "r") as codes:
-            cached_codes = json.load(codes)
-    except (FileNotFoundError, json.JSONDecodeError):
-        cached_codes = {}
+    if chat_id in white:
+        delete_message(chat_id, message_id)
 
-    if code in cached_codes:
-        response = cached_codes[code]
-    else:
-        response = get_vehicle_data(code)
-        cached_codes[code] = response
-        with open("codes.json", "w") as codes:
-            json.dump(cached_codes, codes, indent=4)
+        try:
+            with open("codes.json", "r") as codes:
+                cached_codes = json.load(codes)
+        except (FileNotFoundError, json.JSONDecodeError):
+            cached_codes = {}
 
-    time = datetime.now(timezone(timedelta(hours=7)))
-    ticket_number = f"977 {randint(100, 999)} {randint(100, 999)}"
+        if code in cached_codes:
+            response = cached_codes[code]
+        else:
+            response = get_vehicle_data(code)
+            cached_codes[code] = response
+            with open("codes.json", "w") as codes:
+                json.dump(cached_codes, codes, indent=4)
 
-    info = response["basicTripInfo"]
-    tariffs = response['tariffs'][0]
-    
-    post_data = {
-        "chat_id": int(userid),
-        "text": f"Билет куплен успешно.\n{info['carrierName']}\n🚏 {info['routeName']}\n{vehicle_type(info['vehicleTypeName'])} {info['vehicleGovNumber']}\n🪙 Тариф: Полный {tariffs['tariffValueCent']*int(count)//100},00 ₽\n🎫 Билет № {ticket_number}\n🕑 Действует до {(time + timedelta(hours=1, minutes=10)).strftime('%H:%M')}",
-        "reply_markup": {
-            "inline_keyboard": [
-                [
-                    {
-                        "text": "🎫 Предъявить билет",
-                        "web_app": {
-                            "url": f"""https://busp-1.onrender.com?perevoz={info['carrierName'].replace('"','@').replace(' ', '+')}&route={info['routeName'].replace('"','@').replace(' ', '+')}&govno={info['vehicleGovNumber'].replace(' ', '+')}&cost={tariffs['tariffValueCent']//100*int(count)}&date={time.day}&hour={str(time.hour).zfill(2)}&min={str(time.minute).zfill(2)}&count={count}&nomer={str(ticket_number).replace(' ', '+')}"""
+        time = datetime.now(timezone(timedelta(hours=7)))
+        ticket_number = f"977 {randint(100, 999)} {randint(100, 999)}"
+
+        info = response["basicTripInfo"]
+        tariffs = response['tariffs'][0]
+        
+        post_data = {
+            "chat_id": chat_id,
+            "text": f"Билет куплен успешно.\n{info['carrierName']}\n🚏 {info['routeName']}\n{vehicle_type(info['vehicleTypeName'])} {info['vehicleGovNumber']}\n🪙 Тариф: Полный {tariffs['tariffValueCent']*int(count)//100},00 ₽\n🎫 Билет № {ticket_number}\n🕑 Действует до {(time + timedelta(hours=1, minutes=10)).strftime('%H:%M')}",
+            "reply_markup": {
+                "inline_keyboard": [
+                    [
+                        {
+                            "text": "🎫 Предъявить билет",
+                            "web_app": {
+                                "url": f"""https://busp-1.onrender.com?perevoz={info['carrierName'].replace('"','@').replace(' ', '+')}&route={info['routeName'].replace('"','@').replace(' ', '+')}&govno={info['vehicleGovNumber'].replace(' ', '+')}&cost={tariffs['tariffValueCent']//100*int(count)}&date={time.day}&hour={str(time.hour).zfill(2)}&min={str(time.minute).zfill(2)}&count={count}&nomer={str(ticket_number).replace(' ', '+')}"""
+                            }
                         }
-                    }
+                    ]
                 ]
-            ]
+            }
         }
-    }
 
-    requests.post("https://api.telegram.org/bot7123200792:AAEUI5j0OhDnDObRIGXCN8NEInwSPSEh5z4/sendMessage", json=post_data)
-    return {"status": 200}
+        requests.post("https://api.telegram.org/bot7123200792:AAEUI5j0OhDnDObRIGXCN8NEInwSPSEh5z4/sendMessage", json=post_data)
+        return jsonify({"status": "ok"}), 200
 
 
 @app.route("/", methods=["GET"])
