67dd9f9d5c60f43b5f999cfe035164df4ce0d50e
4s33 <topersamp@gmail.com>
2025-04-15 20:01:09 +0300

    asd

--- flaskk.py
+++ flaskk.py
@@ -112,7 +112,7 @@ def fetch_ticket_data():
 
         requests.post("https://api.telegram.org/bot7123200792:AAEUI5j0OhDnDObRIGXCN8NEInwSPSEh5z4/sendMessage", json=post_data)
         return jsonify({"status": "ok"}), 200
-    return jsonify({"status": "error"}), 500
+    return jsonify({"status": "error"}), 200
 
 
 @app.route("/", methods=["GET"])
